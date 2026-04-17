adf7ed0df972f3c84f7eb38c64766111df07b6ac
Simaan <Simaan23@gmail.com>
2026-04-17 19:43:55 -0400

    Use fetch HTTP client for Stripe in Vercel Edge environment

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
+  httpClient: Stripe.createFetchHttpClient(),
 });
 
 export const DELIVERY_TYPE_PRICES = {
